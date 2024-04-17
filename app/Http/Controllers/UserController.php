b4b7b802b8cf26914720d8893945af6b52381f91
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-04-17 16:45:55 +0800

    new commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,3 +51,5 @@ class UserController extends Controller
 
     
 }
+
+
--- resources/views/operations/managenotice.blade.php
+++ resources/views/operations/managenotice.blade.php
@@ -2,108 +2,92 @@
 
 @section('content')
 <div class="content-wrapper">
+
+
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
+                <div class="col-sm-4">
                     <ol class="breadcrumb float-sm-left">
-                        <li class="breadcrumb-item"><i class="nav-icon far fa-plus-square"></i> Admin Control</li>
-                        <li class="breadcrumb-item"><i class="fas fa-user-friends nav-iconn"></i> User Lists</li>
-                        <li class="breadcrumb-item active"><i class="fas fa-user-cog nav-icon"></i> Admin</li>
+                        <li class="breadcrumb-item"><i class="fas fa-bell nav-icon"></i> Notice</a></li>
+                        <li class="breadcrumb-item active"><i class="fas fa-tasks nav-icon"></i> Manage Notice</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
 
+
+
     <div class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Title</th>
-                                        <th>Content</th>
-                                        <th>Created At</th>
-                                        <th style="width: 150px;">Actions</th> <!-- Adjusted width for Actions column -->
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($notices as $notice)
-                                    <tr>
-                                        <td>{{ $notice->notice_title }}</td>
-                                        <td>{{ $notice->notice_content }}</td>
-                                        <td>{{ $notice->created_at }}</td>
-                                        <td>
-                                            <div class="d-flex justify-content-between">
-                                                <button class="btn btn-warning btn-sm mr-1" onclick="showEditModal({{ $notice->id }})">Edit</button>
-                                                <form method="post" action="{{ '/deletenotice/' . $notice->id }}" style="display: inline-block;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                                </form>
+
+            <div id="notice"></div>
+            <div class="card">
+                <div class="card-header">
+                    Notice
+                </div>
+                <div class="card-body">
+                    <div class="container">
+                        @foreach ($notices as $notice)
+                        <div class="row justify-content-between border p-3">
+                            <div class="col-md-8">
+                                <h4>{{ $notice['notice_title'] }}</h4>
+                                <p>{{ $notice['notice_content'] }}</p>
+                                <small class="text-muted">Created at: {{ $notice['created_at'] }}</small>
+                            </div>
+                            <div class="col-md-4">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#modal-{{ $notice['id'] }}">
+                                    Edit
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="modal-{{ $notice['id'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                            <form method="post" action="managenotice/{{ $notice['id'] }}">
+                                                @csrf
+                                                @method('PUT')
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="title" class="form-label">Title</label>
+                                                        <input type="text" name="notice_title" class="form-control" id="notice_title" value="{{ $notice['notice_title'] }}">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="content" class="form-label">Content</label>
+                                                        <input type="text" name="notice_content" class="form-control" id="notice_content" value="{{ $notice['notice_content'] }}" style="height: 300px">
+                                                    </div>
+                                                </div>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form method="post" action={{ 'deletenotice/' . $notice['id'] }}>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </div>
                         </div>
+                        @endforeach
+                        <script src="js/app.js"></script>
                     </div>
                 </div>
             </div>
+            {{ $notices->links() }}
+
         </div>
     </div>
+
 </div>
-
-<!-- Edit Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editModalLabel">Edit Notice</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form id="editForm" action="" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="editTitle">Title</label>
-                        <input type="text" class="form-control" id="editTitle" name="notice_title" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editContent">Content</label>
-                        <input type="text" class="form-control" id="editContent" name="notice_content" required>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">Save changes</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<script src="js/app.js"></script>
-
-<script>
-    function showEditModal(noticeId) {
-        console.log('Opening edit modal for notice with ID:', noticeId);
-        // Fetch notice data from server and fill in modal fields
-        $.get('/notices/' + noticeId, function(data) {
-            $('#editForm').attr('action', '/notices/' + noticeId);
-            $('#editTitle').val(data.notice_title);
-            $('#editContent').val(data.notice_content);
-            $('#editModal').modal('show');
-        });
-    }
-</script>
-
 @endsection
